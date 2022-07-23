520cb2b4c1a3bdd54cb7d376d5296b69546c7399
jagaimo0831 <ken.e.ft13@gmail.com>
2022-07-23 21:15:51 +0900

    [add] ESCでのポーズメニュー

---
+++ GJ20220723_TeamC_UProject/Assets/Personal/endo/Script/PauseMenu.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject _PausePanel;
+    // [SerializeField] private Button _PauseButton;
+    [SerializeField] private Button _ResumeButton;
+    [SerializeField] private Button _StageSelectButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _PausePanel.SetActive(false);
+        // _PauseButton.onClick.AddListener(pauseGame);
+        _ResumeButton.onClick.AddListener(resumeGame);
+        _StageSelectButton.onClick.AddListener(goToSelectStage);
+    }
+
+    void Update() 
+    {
+        // キー入力の監視
+        getKeyInput();
+    }
+
+    // ポーズ時の挙動
+    private void pauseGame()
+    {
+        // 時間停止
+        Time.timeScale = 0;
+        _PausePanel.SetActive(true);
+    }
+
+    // ポーズ終了の挙動
+    private void resumeGame()
+    {
+        // 時間停止終了
+        Time.timeScale = 1;
+        _PausePanel.SetActive(false);
+    }   
+
+    // ステージ画面への遷移ボタンの挙動
+    private void goToSelectStage()
+    {
+        // 時間停止終了
+        Time.timeScale = 1;
+        _PausePanel.SetActive(false);
+    }  
+
+    // キー入力の取得
+    private void getKeyInput()
+    {
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            pauseGame();
+        }        
+    }
+}
