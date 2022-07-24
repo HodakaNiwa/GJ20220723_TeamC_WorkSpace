84e15e023d98466d46f4bf38d44983e49d51196e
tsukasa <freddiesumiz.3@gmail.com>
2022-07-24 14:00:55 +0900

    Stage1にPL配置

---
+++ GJ20220723_TeamC_UProject/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform PlayerTransform;
+    [SerializeField]
+    private Vector3 Offset = new Vector3(0.0f, 3.0f, 0.0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = PlayerTransform.position + Offset;
+    }
+}
